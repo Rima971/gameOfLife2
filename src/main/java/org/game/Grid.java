a0f9d7aea894d770fd5bbffbeb5eb0bb641b9d77
Rima971 <madhurima.poddar_ftc@external.swiggy.in>
2024-02-01 11:58:15 +0530

    test grid takes right seeding percentage and populates grid with right number of cells

---
+++ src/main/java/org/game/Grid.java
@@ -0,0 +1,80 @@
+package org.game;
+
+import org.game.enums.Event;
+import org.game.interfaces.ISubscriber;
+
+import java.security.InvalidParameterException;
+
+public class Grid implements ISubscriber {
+    private final int rows, columns, initiallyRequiredCellsCount;
+    public int cellsCount = 0;
+    private final GridItem[][] grid;
+    public Grid(int rows, int columns, double seedingPercentage){
+        if (rows <=0 || columns <=0 || seedingPercentage<=0) throw new InvalidParameterException("A grid cannot be created with a non-positive row, column or seedingPercentage");
+        if (seedingPercentage>=1) throw new InvalidParameterException("A grid cannot be created with a seedingPercentage beyond the range of 0-1");
+        this.rows = rows;
+        this.columns = columns;
+        this.initiallyRequiredCellsCount =  (int) Math.round(seedingPercentage * rows * columns);
+        this.grid = new Cell[rows][columns];
+        this.initialize();
+    }
+
+
+    private void initialize(){
+        for (int i=0; i<this.rows; i++){
+            for (int j=0; j<this.columns; j++){
+                this.grid[i][j] = new Space(i,j);
+            }
+        }
+        this.populateCellsRandomly();
+        this.subscribe(Event.CELL_DESTROY, this);
+    }
+    private void populateCellsRandomly(){
+        while (this.cellsCount < this.initiallyRequiredCellsCount){
+            int randomRow = this.getRandomRow();
+            int randomColumn = this.getRandomColumn();
+            if (this.grid[randomRow][randomColumn].getClass() == Cell.class){
+                continue;
+            }
+            this.grid[randomRow][randomColumn] = new Cell(randomRow, randomColumn);
+            this.cellsCount++;
+        }
+    }
+
+    private int getRandomRow(){
+        return (int) Math.floor(Math.random()*this.rows);
+    }
+
+    private int getRandomColumn(){
+        return (int) Math.floor(Math.random()*this.columns);
+    }
+
+    @Override
+    public void onEvent(Event event, Object o) {
+        if (o.getClass() != String.class) return;
+        String[] coordinates = ((String) o).split(" ");
+        int row = Integer.parseInt(coordinates[0]);
+        int column = Integer.parseInt(coordinates[1]);
+        switch (event){
+            case Event.CELL_DESTROY:
+                this.grid[row][column] = new Space(row, column);
+                this.cellsCount--;
+            case Event.CELL_PRODUCE:
+                this.grid[row][column] = new Cell(row, column);
+                this.cellsCount++;
+        }
+
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder output = new StringBuilder();
+        for (GridItem[] gridRow: this.grid){
+            for (GridItem item: gridRow){
+                output.append(item.toString());
+            }
+            output.append("\n");
+        }
+        return output.toString();
+    }
+}
--- src/test/java/GridTest.java
+++ src/test/java/GridTest.java
@@ -1,8 +1,9 @@
-import org.example.Grid;
+import org.game.Grid;
 import org.junit.Test;
 
 import java.security.InvalidParameterException;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -19,5 +20,33 @@ public class GridTest {
         assertThrows(InvalidParameterException.class, ()->new Grid(-16, -20, 0.1));
         assertThrows(InvalidParameterException.class, ()->new Grid(0, 0, 0.1));
         assertThrows(InvalidParameterException.class, ()->new Grid(1, 2, -0.7));
+    }
+
+    @Test
+    public void throwsExceptionOnPassingSeedingPercentageBeyondTheRangeOf0And1(){
+        assertThrows(InvalidParameterException.class, ()->new Grid(2, 3, 0));
+        assertThrows(InvalidParameterException.class, ()->new Grid(12, 23, 1));
+        assertThrows(InvalidParameterException.class, ()->new Grid(0, 0, 0.1));
+    }
+
+    @Test
+    public void grid_of_20x20_with_point2_seeding_percentage_generates_80_alive_cells_initially(){
+        Grid grid = new Grid(20, 20, 0.2);
+        // round(0.2 * 20 * 20)
+        assertEquals(80, grid.cellsCount);
+    }
+
+    @Test
+    public void grid_of_7x11_with_point1_seeding_percentage_generates_8_alive_cells_initially(){
+        Grid grid = new Grid(7, 11, 0.1);
+        // round( 0.1 * 7 * 11 )
+        assertEquals(8, grid.cellsCount);
+    }
+
+    @Test
+    public void grid_of_4x11_with_point1_seeding_percentage_generates_4_alive_cells_initially(){
+        Grid grid = new Grid(4, 11, 0.1);
+        // round( 0.1 * 4 * 11 )
+        assertEquals(4, grid.cellsCount);
     }
 }
