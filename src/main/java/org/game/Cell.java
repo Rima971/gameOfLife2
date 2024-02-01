be6a975ac6b362bcbbade5406646a1c474ae2dfa
Rima971 <madhurima.poddar_ftc@external.swiggy.in>
2024-02-02 02:12:57 +0530

    added tests to check response of cells to events

---
+++ src/main/java/org/game/Cell.java
@@ -0,0 +1,39 @@
+package org.game;
+
+import org.game.enums.Event;
+import org.game.enums.Symbol;
+
+public class Cell extends GridItem {
+    public Cell(int row, int column){
+        super(row, column);
+        this.subscribe(Event.BROADCAST_STATE, this);
+        this.subscribe(Event.UPDATE_STATE, this);
+    }
+
+    @Override
+    public void onEvent(Event event, Object o) {
+        super.onEvent(event, o);
+        switch (event){
+            case Event.BROADCAST_STATE :
+                this.publish(Event.CELL_STATE, this);
+                break;
+            case Event.UPDATE_STATE:
+                this.update();
+        }
+    }
+    private void update(){
+        if (this.getSurroundingCellsCount()<2 || this.getSurroundingCellsCount()>3){
+            this.destroy();
+        }
+        this.resetSurroundingCellCount();
+    }
+
+    private void destroy(){
+        this.publish(Event.CELL_DESTROY, super.toString());
+    }
+
+    @Override
+    public String toString(){
+        return Symbol.ALIVE.representation;
+    }
+}
--- src/test/java/CellTest.java
+++ src/test/java/CellTest.java
@@ -1,21 +1,33 @@
-import org.example.Cell;
+import org.game.Cell;
+import org.game.enums.Event;
+import org.game.stubs.PublisherStub;
+import org.game.stubs.SubscriberStub;
 import org.junit.Test;
 
-import java.security.InvalidParameterException;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class CellTest {
+    private final PublisherStub publisherStub = new PublisherStub();
+    private final SubscriberStub subscriberStub = new SubscriberStub();
     @Test
     public void successfullyInstantiateCell(){
         assertDoesNotThrow(()->new Cell(2,3));
     }
 
     @Test
-    public void throwsExceptionOnInstantiatingWithNegativeCoordinates(){
-        assertThrows(InvalidParameterException.class, ()->new Cell(-2,3));
-        assertThrows(InvalidParameterException.class, ()->new Cell(12,-3));
-        assertThrows(InvalidParameterException.class, ()->new Cell(-12,-3));
+    public void publishesItsStateOnReceivingBroadcastStateEvent(){
+        Cell cell = new Cell(3,4);
+        this.publisherStub.publish(Event.BROADCAST_STATE, null);
+        assertTrue(this.subscriberStub.check(Event.CELL_STATE, cell));
+    }
+
+    @Test
+    public void updatesItsStateOnReceivingUpdateStateEvent(){
+        new Cell(3,4);
+        publisherStub.publish(Event.UPDATE_STATE, null);
+        assertTrue(subscriberStub.check(Event.CELL_DESTROY, "3 4"));
     }
 }
